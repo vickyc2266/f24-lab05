6343bfdae56a59cfd56ec454ee1419e8f691f2e5
Vicky Chen <vickyc@andrew.cmu.edu>
2024-09-27 11:34:54 -0400

    added code into the Frogger.java file to update where the methods are called from

--- Java/src/main/java/frogger/Frogger.java
+++ Java/src/main/java/frogger/Frogger.java
@@ -36,24 +36,30 @@ public class Frogger {
      */
     public boolean move(boolean forward) {
         int nextPosition = this.position + (forward ? 1 : -1);
-        if (!isValid(nextPosition) || isOccupied(nextPosition)) {
+        // if (!isValid(nextPosition) || isOccupied(nextPosition)) {
+        //     return false;
+        // }
+        // this.position = nextPosition;
+        // return true;
+        if (!road.isValid(nextPosition) || road.isOccupied(nextPosition)) {
             return false;
         }
         this.position = nextPosition;
         return true;
     }
 
+
     // TODO: Do you notice any issues here?
-    public boolean isOccupied(int position) {
-        boolean[] occupied = this.road.getOccupied();
-        return occupied[position];
-    }
+    // public boolean isOccupied(int position) {
+    //     boolean[] occupied = this.road.getOccupied();
+    //     return occupied[position];
+    // }
     
-    public boolean isValid(int position) {
-        if (position < 0) return false;
-        boolean[] occupied = this.road.getOccupied();
-        return position < occupied.length;
-    }
+    // public boolean isValid(int position) {
+    //     if (position < 0) return false;
+    //     boolean[] occupied = this.road.getOccupied();
+    //     return position < occupied.length;
+    // }
 
     /**
      * Records Frogger to the list of records.
